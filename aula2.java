125f0e8cf3adc6e63927581d69caa276977c5c33
GabrielBonatto <88838468+GabrielBonatto@users.noreply.github.com>
2021-08-26 21:52:58 -0300

    aula 2 java

---
+++ aula2.java
@@ -0,0 +1,9 @@
+public class AlunoReset {
+
+        public static void main(String[] args) {
+            System.out.println("Sou aluno do CWI RESET Me chamo Gabriel!");
+        }
+    
+    }
+    
+
